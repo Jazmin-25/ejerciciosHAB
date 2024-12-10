c3897f50c62236db1d6cb3fd64df0d050be1af9e
Jazmin-25 <jaz.islasg@gmail.com>
2024-12-10 11:31:53 -0600

    Ejercicio de Variables y Operadores dia 1

---
+++ 01-Java-Basico/VariablesOperadores/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//int base = 20;
+//int altura = 18;
+double base = 15.32;
+int altura = 5;
+double resultado = (double) (base * altura) / 2;
+
+        System.out.println("El área del tringulo es: " + resultado);
+        }
+    }
