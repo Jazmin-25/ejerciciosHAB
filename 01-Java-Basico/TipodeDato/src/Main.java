0abcd4fa77e5a5cb6612b570eeea885162ddec21
Jazmin-25 <jaz.islasg@gmail.com>
2024-12-10 11:13:49 -0600

    Se sube el segundo ejercicio

---
+++ 01-Java-Basico/TipodeDato/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int edad = 29;
+        String nombres = "Ariadna";
+        boolean bandera = true;
+        double estatura = 1.49;
+        String ocupacion = "programadora";
+
+        System.out.println("Hola " + nombres + " tu edad es " + edad + " años" + " ¿puedo ayudar en algo mas?");
+    }
+}
